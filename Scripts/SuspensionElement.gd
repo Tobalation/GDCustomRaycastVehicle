ac746b33e83edff279eb275e55b688d1453b6ccf
Dan Suwanseree <ayw.dansuwanseree@gmail.com>
2019-11-03 21:07:29 +0700

    Improved suspension, added new cam rig
    Better sping code and added updated camera rig

--- Scripts/SuspensionElement.gd
+++ Scripts/SuspensionElement.gd
@@ -1,14 +1,13 @@
 extends RayCast
 # control variables
-export var springForce : float = 50.0
-export var damping : float = 8.0
+export var springForce : float = 80.0
+export var stifness : float = 0.8
+export var damping : float = 0.08
 export var Xtraction : float = 20.0
 export var Ztraction : float = 2.0
-export var latSlideTreshhold : float = 0.3
-# refernce to parent body so we can apply forces
+# private variables
 var parentBody : RigidBody
 var parentVelocity : Vector3
-# variable to hold information for damping
 var previousDistance : float = abs(cast_to.y)
 var previousHit : Vector3 = Vector3()
 
@@ -18,6 +17,7 @@ func applyDriveForce(force : Vector3) -> void:
 
 func _ready() -> void:
 	parentBody = get_parent()
+	add_exception(parentBody)
 	
 func _physics_process(delta) -> void:
 	if is_colliding():
@@ -28,14 +28,15 @@ func _physics_process(delta) -> void:
 		var ZVelocity = global_transform.basis.xform_inv(velocity).z
 		var XVelocity = global_transform.basis.xform_inv(velocity).x
 		# axis deceleration forces
-		var sideWaysForce = -global_transform.basis.x * XVelocity * Xtraction * delta
-		var decelerationForce = -global_transform.basis.z * ZVelocity * Ztraction * delta
+		var XForce = -global_transform.basis.x * XVelocity * Xtraction * delta
+		var ZForce = -global_transform.basis.z * ZVelocity * Ztraction * delta
 		# apply spring force with damping
 		var curDistance = (global_transform.origin - get_collision_point()).length()
-		var suspensionForce = springForce * (abs(cast_to.y) - curDistance) + damping * (previousDistance - curDistance)/delta
+		var suspensionForce = (stifness * (abs(cast_to.y) - curDistance) + damping * (previousDistance - curDistance)/delta) * springForce
+		if suspensionForce < 0.0 : suspensionForce = 0.0
 		var suspensionImpulse = global_transform.basis.y * suspensionForce * delta
 		
-		var finalForce = suspensionImpulse + sideWaysForce + decelerationForce
+		var finalForce = suspensionImpulse + XForce + ZForce
 		
 		# negate suspension forces to stop sliding
 		# ref: https://github.com/shadowmage45/KSPWheel/blob/master/VSProject/KSPWheel/Component/KSPWheelCollider.cs
@@ -47,8 +48,8 @@ func _physics_process(delta) -> void:
 		
 		if GameState.debugMode:
 			DrawLine3D.DrawRay(get_collision_point(),suspensionImpulse,Color(0,255,0))
-			DrawLine3D.DrawRay(get_collision_point(),sideWaysForce,Color(255,0,0))
-			DrawLine3D.DrawRay(get_collision_point(),decelerationForce,Color(0,0,255))
+			DrawLine3D.DrawRay(get_collision_point(),XForce,Color(255,0,0))
+			DrawLine3D.DrawRay(get_collision_point(),ZForce,Color(0,0,255))
 			
 			DrawLine3D.DrawRay(get_collision_point(),uphillVec,Color(0,0,0))
 			DrawLine3D.DrawRay(get_collision_point(),antiSlideForce,Color(255,255,255))
