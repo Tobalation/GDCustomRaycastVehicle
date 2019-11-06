2795b1e43497a52b98ccb0c16f03aa4c4ac8ac78
Dan Suwanseree <ayw.dansuwanseree@gmail.com>
2019-11-06 19:46:14 +0700

    Fixed sliding, changed friction concept
    Friction now a coefficient, applies based on weight.
    When stopped, suspension force applies vertically

--- Scripts/RayDriver.gd
+++ Scripts/RayDriver.gd
@@ -5,17 +5,18 @@ export var maxForce : float = 500.0
 export var springForce : float = 80.0
 export var stifness : float = 0.8
 export var damping : float = 0.08
-export var Xtraction : float = 20.0
-export var Ztraction : float = 2.0
-export var counterSliding : bool = true
+export var Xtraction : float = 1.0
+export var Ztraction : float = 0.1
 
 # public variables
 var instantLinearVelocity : Vector3
+var braking : bool = false
 
 # private variables
 var parentBody : RigidBody
 var previousDistance : float = abs(cast_to.y)
 var previousHit : Vector3 = Vector3()
+var speedThreshold : float = 0.2
 
 # function for applying drive force to parent body (if grounded)
 func applyDriveForce(force : Vector3) -> void:
@@ -37,28 +38,18 @@ func _physics_process(delta) -> void:
 		var ZVelocity = global_transform.basis.xform_inv(instantLinearVelocity).z
 		var XVelocity = global_transform.basis.xform_inv(instantLinearVelocity).x
 		# axis deceleration forces
-		var XForce = -global_transform.basis.x * XVelocity * Xtraction * delta
-		var ZForce = -global_transform.basis.z * ZVelocity * Ztraction * delta
+		var XForce = -global_transform.basis.x * XVelocity * (parentBody.weight * parentBody.gravity_scale)/parentBody.rayElements.size() * Xtraction * delta
+		var ZForce = -global_transform.basis.z * ZVelocity * (parentBody.weight * parentBody.gravity_scale)/parentBody.rayElements.size() * Ztraction * delta
 		# apply spring force with damping force, has very tiny jitter when near stable
 		var curDistance = (global_transform.origin - get_collision_point()).length()
 		var FSpring = stifness * (abs(cast_to.y) - curDistance) 
 		var FDamp = damping * (previousDistance - curDistance)/delta
 		var suspensionForce = clamp((FSpring + FDamp) * springForce,0,maxForce)
 		var suspensionImpulse = global_transform.basis.y * suspensionForce * delta
+		if braking:
+			suspensionImpulse = Vector3.UP * suspensionForce * delta
 		# final impulse force vector to be applied
 		var finalForce = suspensionImpulse + XForce + ZForce
-		
-		# negate suspension forces to stop sliding
-		# ref: https://github.com/shadowmage45/KSPWheel/blob/master/VSProject/KSPWheel/Component/KSPWheelCollider.cs
-		if counterSliding:
-			var gravNormDot = get_collision_normal().dot(Vector3.DOWN)
-			var uphillVec = (get_collision_normal().cross(Vector3.DOWN)).cross(get_collision_normal())
-			var slopeLatDot = uphillVec.dot(global_transform.basis.x)
-			var antiSlideForce = global_transform.basis.x * finalForce.y * gravNormDot * slopeLatDot
-			finalForce += antiSlideForce
-			if GameState.debugMode:
-				DrawLine3D.DrawRay(get_collision_point(),uphillVec,Color(0,0,0))
-				DrawLine3D.DrawRay(get_collision_point(),antiSlideForce,Color(255,255,255))
 			
 		# draw debug lines using the awesome DrawLine3D library
 		if GameState.debugMode:
