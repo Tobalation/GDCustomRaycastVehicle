22562500d3b47abb4f7b83b551469b7cc26a28b3
Dan Suwanseree <ayw.dansuwanseree@gmail.com>
2019-11-07 01:06:39 +0700

    Proper simulation of friction, improved test level
    New test level objects, finally figured out proper friction

--- Scripts/RayDriver.gd
+++ Scripts/RayDriver.gd
@@ -7,7 +7,6 @@ export var stifness : float = 0.8
 export var damping : float = 0.08
 export var Xtraction : float = 1.0
 export var Ztraction : float = 0.1
-export var staticFrictionThreshold : float = 0.5
 
 # public variables
 var instantLinearVelocity : Vector3
@@ -16,7 +15,6 @@ var instantLinearVelocity : Vector3
 var parentBody : RigidBody
 var previousDistance : float = abs(cast_to.y)
 var previousHit : Vector3 = Vector3()
-var speedThreshold : float = 0.2
 
 # function for applying drive force to parent body (if grounded)
 func applyDriveForce(force : Vector3) -> void:
@@ -34,23 +32,28 @@ func _physics_process(delta) -> void:
 		# obtain instantaneaous linear velocity
 		var curHit = get_collision_point()
 		instantLinearVelocity = (curHit - previousHit) / delta
-		# obtain axis velocity
-		var ZVelocity = global_transform.basis.xform_inv(instantLinearVelocity).z
-		var XVelocity = global_transform.basis.xform_inv(instantLinearVelocity).x
-		# axis deceleration forces
-		var XForce = -global_transform.basis.x * XVelocity * (parentBody.weight * parentBody.gravity_scale)/parentBody.rayElements.size() * Xtraction * delta
-		var ZForce = -global_transform.basis.z * ZVelocity * (parentBody.weight * parentBody.gravity_scale)/parentBody.rayElements.size() * Ztraction * delta
+		
 		# apply spring force with damping force, has very tiny jitter when near stable
 		var curDistance = (global_transform.origin - get_collision_point()).length()
 		var FSpring = stifness * (abs(cast_to.y) - curDistance) 
 		var FDamp = damping * (previousDistance - curDistance)/delta
 		var suspensionForce = clamp((FSpring + FDamp) * springForce,0,maxForce)
 		var suspensionImpulse = global_transform.basis.y * suspensionForce * delta
-		if parentBody.linear_velocity.length() < staticFrictionThreshold:
-			suspensionImpulse = Vector3.UP * suspensionForce * delta
+		
+		# obtain axis velocity
+		var ZVelocity = global_transform.basis.xform_inv(instantLinearVelocity).z
+		var XVelocity = global_transform.basis.xform_inv(instantLinearVelocity).x
+		
+		# axis deceleration forces
+		var XForce = -global_transform.basis.x * XVelocity * (parentBody.weight * parentBody.gravity_scale)/parentBody.rayElements.size() * Xtraction * delta
+		var ZForce = -global_transform.basis.z * ZVelocity * (parentBody.weight * parentBody.gravity_scale)/parentBody.rayElements.size() * Ztraction * delta
+		
+		# counter sliding by negating off axis suspension impulse
+		XForce.x -= suspensionImpulse.x
+		ZForce.z -= suspensionImpulse.z
+		
 		# final impulse force vector to be applied
 		var finalForce = suspensionImpulse + XForce + ZForce
-			
 		# draw debug lines using the awesome DrawLine3D library
 		if GameState.debugMode:
 			DrawLine3D.DrawRay(get_collision_point(),suspensionImpulse,Color(0,255,0))
