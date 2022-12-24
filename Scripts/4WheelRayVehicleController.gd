33e8fd759cd2dd5ffd5b43d5b63d3efd23fa12a2
Dan Suwanseree <ayw.dansuwanseree@gmail.com>
2022-12-25 02:26:17 +0700

    Reworked tracked vehicle controller
    - Renamed throttle and brake to forward and reverse
    - Renaming of some parts of controller scripts
    - Tracked vehicle controller reworked. Works more realistically and as intended

--- Scripts/4WheelRayVehicleController.gd
+++ Scripts/4WheelRayVehicleController.gd
@@ -3,21 +3,24 @@ extends RigidBody
 # control variables
 export(float) var enginePower : float = 150.0
 export(Curve) var torqueCurve : Curve
+
 export(float) var maxSpeedKph : float = 100.0
 export(float) var maxReverseSpeedKph : float = 20.0
+
 export(float) var maxBrakingCoef : float = 0.05
-export(float) var rollingResistance : float = 0.0001
+export(float) var rollingResistance : float = 0.001
 
 export(float) var steeringAngle : float = 30.0
 export(float) var steerSpeed : float = 15.0
 export(float) var maxSteerLimitRatio : float = 0.95
 export(float) var steerReturnSpeed : float = 30.0
+
 export(float) var autoStopSpeedMS : float = 1.0
 
-onready var fl_ray: Spatial = $FL_ray
-onready var fr_ray: Spatial = $FR_ray
+onready var frontLeftElement: Spatial = $FL_ray
+onready var frontRightElement: Spatial = $FR_ray
 
-var rayElements : Array = []
+var driveElements : Array = []
 var drivePerRay : float = enginePower
 
 var currentDrivePower : float = 0.0
@@ -28,12 +31,12 @@ var currentSpeed : float = 0.0
 
 func _handle_physics(delta) -> void:
 	# 4WD with front wheel steering
-	for driveElement in rayElements:
+	for driveElement in driveElements:
 		var finalForce : Vector3 = Vector3.ZERO
 		var finalBrake : float = rollingResistance
 		
 		# get throttle axis
-		var forwardDrive : float = Input.get_axis("brake", "throttle")
+		var forwardDrive : float = Input.get_axis("reverse", "forward")
 		# get steering axis
 		var steering : float = Input.get_axis("steer_left", "steer_right")
 		
@@ -55,8 +58,8 @@ func _handle_physics(delta) -> void:
 		var maxSteerRatio : float = range_lerp(currentSpeed * 3.6, 0, maxSpeedKph, 0, maxSteerLimitRatio)
 		maxSteerAngle = (1 - maxSteerRatio) * steeringAngle
 		currentSteerAngle = clamp(currentSteerAngle, -maxSteerAngle, maxSteerAngle)
-		fr_ray.rotation_degrees.y = currentSteerAngle
-		fl_ray.rotation_degrees.y = currentSteerAngle
+		frontRightElement.rotation_degrees.y = currentSteerAngle
+		frontLeftElement.rotation_degrees.y = currentSteerAngle
 
 		# brake if movement opposite indended direction
 		if sign(currentSpeed) != sign(forwardDrive) && !is_zero_approx(currentSpeed) && forwardDrive != 0:
@@ -84,9 +87,9 @@ func _ready() -> void:
 	# setup array of drive elements and setup drive power
 	for node in get_children():
 		if node is DriveElement:
-			rayElements.append(node)
-	drivePerRay = enginePower / rayElements.size()
-	print("Found %d drive elements connected to wheeled vehicle, setting to provide %.2f force each." % [rayElements.size(), drivePerRay]) 
+			driveElements.append(node)
+	drivePerRay = enginePower / driveElements.size()
+	print("Found %d drive elements connected to wheeled vehicle, setting to provide %.2f force each." % [driveElements.size(), drivePerRay]) 
 	
 func _physics_process(delta) -> void:
 	# calculate forward speed
--- Scripts/TrackedRaycastVehicleController.gd
+++ Scripts/TrackedRaycastVehicleController.gd
@@ -1,19 +1,30 @@
 extends RigidBody
 
+# control mode enumerator
+enum DriveMode {
+	DOUBLE_DIFF,
+	BRAKED_DIFF,
+	DIRECT_FORCES,
+}
+
 # control variables
-export(bool) var neutralSteer : bool = true
+export(DriveMode) var driveTrainMode = DriveMode.DOUBLE_DIFF
 export(bool) var invertSteerWhenReverse : bool = false
+
 export(float) var enginePower : float = 150
 export(Curve) var torqueCurve : Curve
+
 export(float) var maxSpeedKph : float = 65.0
 export(float) var maxReverseSpeedKph : float = 20.0
-export(float) var regularBrakePercent : float = 0.01
-export(float) var steerBrakePercent : float = 0.1
-export(float) var trackBrakingSpeed : float = 0.02
-export(float) var rollingResistance : float = 0.001
+
+export(float) var trackBrakePercent : float = 0.1
+export(float) var trackBrakingSpeed : float = 0.1
+export(float) var rollingResistance : float = 0.02
+
 export(float) var autoStopSpeedMS : float = 1.0
 
-var driveElements : Array = []
+var leftDriveElements : Array = []
+var rightDriveElements : Array = []
 var drivePerRay : float = enginePower
 
 var currentDrivePower : float = 0.0
@@ -24,7 +35,7 @@ var lastSteerValue : float = 0.0
 	
 func _handle_physics(delta) -> void:
 	# get throttle and steering input
-	var forwardDrive : float = Input.get_axis("brake", "throttle")
+	var forwardDrive : float = Input.get_axis("reverse", "forward")
 	var steering : float = Input.get_axis("steer_left", "steer_right")
 	
 	# Invert steering when reversing if enabled
@@ -39,75 +50,110 @@ func _handle_physics(delta) -> void:
 	# reverse
 	elif forwardDrive < 0:
 		speedInterp = range_lerp(abs(currentSpeed), 0.0, maxReverseSpeedKph / 3.6, 0.0, 1.0)
-	# steering drive
+	# steering drive (always at start of curve)
 	elif forwardDrive == 0 && steering != 0:
 		speedInterp = 0
 	
 	# get force from torque curve (based on current speed)
 	currentDrivePower = torqueCurve.interpolate_baked(speedInterp) * drivePerRay
 	
-	# reset steering if opposite input used
-	if sign(steering) != sign(lastSteerValue):
-		currentSteerBrakePower = 0
-	# gradually apply steering and decay if no steering applied
-	if steering != 0:
-		var desiredBrakePower : float = abs(steering) * steerBrakePercent
-		currentSteerBrakePower = move_toward(currentSteerBrakePower, desiredBrakePower, trackBrakingSpeed * delta)
-	else:
-		currentSteerBrakePower = move_toward(currentSteerBrakePower, 0, trackBrakingSpeed * delta)
-	# set last steer value
-	lastSteerValue = steering
-	
-	# set drive for each track driveElement
-	for driveElement in driveElements:
-		var finalForce : Vector3 = Vector3.ZERO
-		var finalBrake : float = rollingResistance
-		var trackSide : int = sign(driveElement.transform.origin.x)
-
-		# no drive inputs, apply parking brake if sitting still
-		if forwardDrive == 0 && steering == 0 && abs(currentSpeed) < autoStopSpeedMS:
-			finalBrake = regularBrakePercent
-			
-		# steering only
-		elif forwardDrive == 0 && steering != 0:
-			if abs(currentSpeed) < autoStopSpeedMS:
-				# handle neutral steering
-				if neutralSteer:
-					finalForce = global_transform.basis.z * currentDrivePower * steering * trackSide
-					finalBrake = rollingResistance
-				else:
-					# handle clutch brake steering
-					if sign(driveElement.transform.origin.x) == sign(steering):
-						finalForce = global_transform.basis.z * currentDrivePower * steering * trackSide
-					else:
-						finalBrake = currentSteerBrakePower * steering * -trackSide
-			else:
-				finalBrake = currentSteerBrakePower * steering * -trackSide
-				
-		else:
-			# regular driving with steering
-			finalForce = global_transform.basis.z * currentDrivePower * clamp(forwardDrive + (steering * trackSide), -1,1)
-			# brake correct side of track
-			if trackSide == sign(-steering * sign(forwardDrive)):
-				finalBrake = currentSteerBrakePower * abs(steering)
-			else:
-				finalBrake = 0
-			# slow down if input opposite drive direction
-			if sign(currentSpeed) != sign(forwardDrive):
-				finalBrake = regularBrakePercent * abs(forwardDrive)
-
-		# apply brake and force
-		driveElement.apply_force(finalForce)
-		driveElement.apply_brake(finalBrake)
+	# handle drive and braking for tracks depending on mode
+	if driveTrainMode == DriveMode.DOUBLE_DIFF:
+		# double differential setup (steer with control of drive force)
+		var leftForce : Vector3 = Vector3.ZERO
+		var rightForce : Vector3 = Vector3.ZERO
+		var braking : float = rollingResistance
 		
+		# calculate drive forces
+		var LDriveFac : float = forwardDrive + steering
+		var RDriveFac : float = forwardDrive + steering * -1
+		leftForce = global_transform.basis.z * currentDrivePower * LDriveFac
+		rightForce = global_transform.basis.z * currentDrivePower * RDriveFac
+		
+		# no brakes during normal driving
+		if LDriveFac != 0 || RDriveFac != 0:
+			braking = 0
+		
+		# slow down if input opposite drive direction
+		if sign(currentSpeed) != sign(forwardDrive):
+			braking = trackBrakePercent * abs(forwardDrive)
+		
+		# apply parking brake if sitting still
+		if forwardDrive == 0 && steering == 0 && abs(currentSpeed) < autoStopSpeedMS:
+			braking = trackBrakePercent
+		
+		# finally apply all forces and braking
+		for element in leftDriveElements:
+			element.apply_force(leftForce)
+			element.apply_brake(braking)
+			
+		for element in rightDriveElements:
+			element.apply_force(rightForce)
+			element.apply_brake(braking)
+		
+	elif driveTrainMode == DriveMode.BRAKED_DIFF:
+		# braked differential setup (steer with braking)
+		var driveForce : Vector3 = Vector3.ZERO
+		var leftBraking : float = rollingResistance
+		var rightBraking : float = rollingResistance
+		
+		# calculate drive force
+		driveForce = global_transform.basis.z * currentDrivePower * forwardDrive
+		
+		# reset steering if opposite input used
+		if sign(steering) != sign(lastSteerValue):
+			currentSteerBrakePower = 0
+		# gradually increase steering brake and decay if no steering applied
+		if steering != 0:
+			var desiredBrakePower : float = abs(steering) * trackBrakePercent
+			currentSteerBrakePower = move_toward(currentSteerBrakePower, desiredBrakePower, trackBrakingSpeed * delta)
+		else:
+			currentSteerBrakePower = move_toward(currentSteerBrakePower, 0, trackBrakingSpeed * delta)
+		# set last steer value
+		lastSteerValue = steering
+		
+		# calculate steering brake
+		if steering < 0:
+			leftBraking = currentSteerBrakePower * abs(steering)
+			rightBraking = 0
+		else:
+			leftBraking = 0
+			rightBraking = currentSteerBrakePower * abs(steering)
+		
+		# slow down if input opposite drive direction
+		if sign(currentSpeed) != sign(forwardDrive):
+			leftBraking = trackBrakePercent * abs(forwardDrive)
+			rightBraking = trackBrakePercent * abs(forwardDrive)
+		
+		# apply parking brake if sitting still
+		if forwardDrive == 0 && steering == 0 && abs(currentSpeed) < autoStopSpeedMS:
+			leftBraking = trackBrakePercent
+			rightBraking = trackBrakePercent
+		
+		# finally apply all forces and braking
+		for element in leftDriveElements:
+			element.apply_force(driveForce)
+			element.apply_brake(leftBraking)
+			
+		for element in rightDriveElements:
+			element.apply_force(driveForce)
+			element.apply_brake(rightBraking)
+		
+	elif driveTrainMode == DriveMode.DIRECT_FORCES:
+		# Drive and turn using direct forces on vehicle body
+		# TODO: this later
+		pass
 
 func _ready() -> void:
 	# setup arrays of drive elements and setup drive power
 	for node in get_children():
 		if node is DriveElement:
-			driveElements.append(node)
-	drivePerRay = enginePower / driveElements.size()
-	print("Found %d track elements connected to vehicle. Each driveElement providing %.2f force each." % [driveElements.size(), drivePerRay])
+			if sign(node.transform.origin.x) > 0:
+				leftDriveElements.append(node)
+			else:
+				rightDriveElements.append(node)
+	drivePerRay = enginePower / (leftDriveElements.size()+rightDriveElements.size())
+	print("Found %d track elements connected to vehicle. Each driveElement providing %.2f force each." % [leftDriveElements.size()+rightDriveElements.size(), drivePerRay])
 	
 func _physics_process(delta) -> void:
 	# calculate forward speed
